0d6826f8212caa1aa0422d7b712927b12d711d3b
Kush <kushpatel124@gmail.com>
2023-01-21 15:32:28 -0500

    commit tree 44c4f7a3e7f6523daaf4624263661f8c1f61b646

---
+++ index.js
@@ -0,0 +1,5 @@
+myEach([1, 2, 3], alert);
+// => alerts each number in turn and returns the original collection
+
+myEach({ one: 1, two: 2, three: 3 }, alert);
+// => alerts each number value in turn and returns the original collection
